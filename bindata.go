0d3674002ae8bdac017a82ea0e3108f462b4aea6
Michael Ryan <mik3.ry4n@gmail.com>
2016-08-09 13:39:26 -0400

    updating bindata to be in the fasttag package
    The auto generated code used package main by default.

--- bindata.go
+++ bindata.go
@@ -3,7 +3,7 @@
 // lexicons/lexicon.txt
 // DO NOT EDIT!
 
-package main
+package fasttag
 
 import (
 	"bytes"
@@ -182,6 +182,7 @@ type bintree struct {
 	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
+
 var _bintree = &bintree{nil, map[string]*bintree{
 	"lexicons": &bintree{nil, map[string]*bintree{
 		"lexicon.txt": &bintree{lexiconsLexiconTxt, map[string]*bintree{}},
@@ -234,4 +235,3 @@ func _filePath(dir, name string) string {
 	cannonicalName := strings.Replace(name, "\\", "/", -1)
 	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
 }
-
